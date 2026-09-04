fbca935f78a4cfee97dd25ed1d2f971eeb2f01e4
Anthony <anthonyhazkour@gmail.com>
2026-09-04 15:33:43 -0400

    Version 942026-2

--- register.py
+++ register.py
@@ -4,23 +4,63 @@ import sqlite3
 connection = sqlite3.connect("users.db")
 cursor = connection.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTs users (
-                        username PRIMARY KEY NOT NULL,
+                        username text PRIMARY KEY NOT NULL,
                         password text NOT NULL,
                         email text NOT NULL,
                         name text NOT NULL,
                         lastname text NOT NULL
                         )
                         """)
-connection.commit()
-connection.close()
+
 
 def register_user(connection, username, password, email, name, lastname):
-    query = "INSERT INTO users (username, password, email, name, lastname) VALUES (?, ?, ?, ?, ?)"
-    try:
-        with connection:
-            connection.execute(query, (username, password, email, name, lastname))
-    except Exception as e:
-        print(e)
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    namecheck = cursor.fetchone()
+
+    cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+    emailcheck = cursor.fetchone()
+
+    if namecheck is not None:
+        print("User already exists")
+    elif emailcheck is not None:
+        print("This Email is already in use")
+    else:
+        query = "INSERT INTO users (username, password, email, name, lastname) VALUES (?, ?, ?, ?, ?)"
+        try:
+            with connection:
+                connection.execute(query, (username, password, email, name, lastname))
+        except Exception as e:
+            print(e)
+
+def user_find(connection, user):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ?", (user,))
+    userinfo = cursor.fetchone()
+    if userinfo is not None:
+        print(f"""We Found the user:
+                Username: {userinfo[0]}
+                Email: {userinfo[2]}
+                Full Name: {userinfo[4]}, {userinfo[3]}""")
+    else:
+        print("User not found")
+
+def login_check(connection, user, password):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ?", (user,))
+    usernamecheck = cursor.fetchone()
+    if usernamecheck is None:
+        print("User not found")
+    else:
+        if password == usernamecheck[1]:
+            print("Login successful")
+            print(usernamecheck)
+        else:
+            print("Incorrect Password")
+            print(usernamecheck)
+
+
 
 itson = True
 
@@ -32,24 +72,27 @@ while itson:
     4.- Exit Program
     """)
 
+    option = int(input("Select an option:"))
+    if option == 1:
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        email = input("Enter your email: ")
+        name = input("Enter your name: ")
+        lastname = input("Enter your last name: ")
+        register_user(connection, username, password, email, name, lastname)
+    elif option == 2:
+        user = input("Enter your username to look up: ")
+        user_find(connection, user)
+    elif option == 3:
+        user = input("Enter your username: ")
+        password = input("Enter your password: ")
+        login_check(connection, user, password)
+    elif option == 4:
+        print("Exiting Program")
+        itson = False
+        connection.close()
+    else: print("Please enter an option from the list above")
 
-
-    try:
-        option = int(input("Select an option:"))
-        if option == 1:
-            username = input("Enter your username: ")
-            password = input("Enter your password: ")
-            email = input("Enter your email: ")
-            name = input("Enter your name: ")
-            lastname = input("Enter your last name: ")
-            register_user(connection, username, password, email, name, lastname)
-        elif option == 4:
-            print("Exiting Program")
-            itson = False
-        else: print("Please enter an option from the list above")
-
-    except:
-        print("This is not a valid option")
 
 
 
